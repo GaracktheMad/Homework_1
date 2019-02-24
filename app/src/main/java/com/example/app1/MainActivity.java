4f2f3786a1d09c480e4a699fa574d24047aa9eff
Peter Vukas <robodude55@hotmail.com>
2019-02-24 13:48:07 -0500

    Added web view example. Displays http://howvaccinescauseautism.org/ which should show the text "They Don't."

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.app1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webthing);
+
+        webView.loadUrl("http://howvaccinescauseautism.org/");
+
     }
 }
