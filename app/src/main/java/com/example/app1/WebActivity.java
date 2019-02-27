cb3ec4b69704b605a9ed2cc536c400eb95379f5c
Peter Vukas <robodude55@hotmail.com>
2019-02-26 19:52:06 -0500

    Now allows users to ask the application via a button if vaccines cause autism, which then takes the user to a web page displaying the correct answer.

--- app/src/main/java/com/example/app1/WebActivity.java
+++ app/src/main/java/com/example/app1/WebActivity.java
@@ -4,23 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class WebActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.createAutism);
+        setContentView(R.layout.activity_web);
+        WebView webView = findViewById(R.id.webthing);
+        webView.loadUrl("http://howvaccinescauseautism.org/");
+        Button btn = findViewById(R.id.finished);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, WebActivity.class));
+                startActivity(new Intent(WebActivity.this, MainActivity.class));
             }
         });
-
-
     }
 }
